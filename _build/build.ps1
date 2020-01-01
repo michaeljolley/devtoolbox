94f78c6b39fef35115fa707aa4bd6ccea6e5063f
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2020-01-01 11:52:58 -0800

    Added a build script to remove Tests before publishing

---
+++ _build/build.ps1
@@ -0,0 +1,8 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$dst = $here -replace "\\_build", "\dist"
+$src = $here -replace "\\_build", "\src"
+if (Test-Path $dst) { Remove-Item -Path $dst -Recurse -Force }
+Get-ChildItem -Path $src -Exclude *.Tests.ps1 -Recurse | Where-Object FullName -NotLike "*mocks*" | ForEach-Object {
+  $destination = $_.FullName -replace "\\src", "\dist"
+  Copy-Item -Path $_.FullName -Destination $destination -Force
+}
